0a85b52a7599b97bf0d2820df6fc30a65884bf2c
Luffy Kim <dldnd67@gmail.com>
2015-04-08 00:15:02 +0900

    구분, 자율선택/기획필수 추가

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/quarters/:id', function(req, res, next) {
     var quarters = [];
     var ids = [];
     var lids = [];
+    var kinds = [];
     var titles = [];
     var authors = [];
     var memnums = [];
@@ -29,7 +30,7 @@ router.get('/quarters/:id', function(req, res, next) {
                     quarter_name = row.quarter;
             });
 
-            db.all("SELECT rowid AS id, title, author, members, memnum FROM lectures where quarter_id=" + quarter_id, function(err, rows) {
+            db.all("SELECT rowid AS id, kind, title, author, members, memnum FROM lectures where quarter_id=" + quarter_id, function(err, rows) {
                 db.close();
                 if (err) {
                     console.log('do not load lectures');
@@ -42,6 +43,7 @@ router.get('/quarters/:id', function(req, res, next) {
                         if (row.members.length > 0)
                             current = row.members.split('^').length;
                         lids.push(row.id);
+                        kinds.push(row.kind);
                         titles.push(row.title);
                         authors.push(row.author);
                         memnums.push(current+'/'+row.memnum);
@@ -51,7 +53,7 @@ router.get('/quarters/:id', function(req, res, next) {
                         else
                             disabled.push('');
                     });
-                    res.render('index', { quarters:quarters, lids:lids, quarter_id:quarter_id, quarter_name:quarter_name, ids:ids, titles:titles, authors:authors, memnums:memnums, disabled:disabled});
+                    res.render('index', { quarters:quarters, lids:lids, quarter_id:quarter_id, quarter_name:quarter_name, ids:ids, kinds:kinds, titles:titles, authors:authors, memnums:memnums, disabled:disabled});
                 }
             });
         }
@@ -62,6 +64,7 @@ router.get('/quarters/:id', function(req, res, next) {
 router.get('/', function(req, res, next) {
     var db = new sqlite3.Database('db/mydb.db');
     var quarters = [];
+    var kinds = [];
     var ids = [];
     var lids = [];
     var titles = [];
@@ -71,7 +74,7 @@ router.get('/', function(req, res, next) {
 
     db.serialize(function() {
         db.run("CREATE TABLE if not exists quarters(quarter TEXT UNIQUE)");
-        db.run("CREATE TABLE if not exists lectures(quarter_id INTEGER, title TEXT, author TEXT, condition TEXT, howto TEXT, members TEXT, memnum INTEGER, one TEXT, two TEXT, three TEXT, four TEXT, five TEXT, six TEXT, seven TEXT, eight TEXT)");
+        db.run("CREATE TABLE if not exists lectures(quarter_id INTEGER, title TEXT, author TEXT, condition TEXT, howto TEXT, members TEXT, memnum INTEGER, one TEXT, two TEXT, three TEXT, four TEXT, five TEXT, six TEXT, seven TEXT, eight TEXT, etc TEXT, purpose TEXT, kind TEXT)");
     });
 
     db.all("SELECT rowid AS id, quarter FROM quarters", function(err, rows) {
@@ -91,7 +94,7 @@ router.get('/', function(req, res, next) {
                quarter_name = row.quarter;
            });
 
-           db.all("SELECT rowid AS id, title, author, members, memnum FROM lectures where quarter_id=" + quarter_id, function(err, rows) {
+           db.all("SELECT rowid AS id, kind, title, author, members, memnum FROM lectures where quarter_id=" + quarter_id, function(err, rows) {
                db.close()
                if (err) {
                    console.log('do not load lectures at home');
@@ -106,13 +109,14 @@ router.get('/', function(req, res, next) {
                        titles.push(row.title);
                        authors.push(row.author);
                        memnums.push(current+'/'+row.memnum);
+                       kinds.push(row.kind);
 
                        if (current == row.memnum)
                            disabled.push('disabled');
                        else
                            disabled.push('');
                    });
-                   res.render('index', { quarters:quarters, quarter_id:quarter_id, quarter_name:quarter_name, lids:lids, ids:ids, titles:titles, authors:authors, memnums:memnums, disabled:disabled});
+                   res.render('index', { quarters:quarters, quarter_id:quarter_id, quarter_name:quarter_name, kinds:kinds, lids:lids, ids:ids, titles:titles, authors:authors, memnums:memnums, disabled:disabled});
                }
            });
         }
@@ -179,6 +183,7 @@ router.get('/lecture/:id', function(req, res, next) {
                     console.log(err);
                     res.render('errorInfo', {});
                 } else {
+                    var kind = rows[0].kind;
                     var title = rows[0].title;
                     var author = rows[0].author;
                     var condition = rows[0].condition;
@@ -202,7 +207,7 @@ router.get('/lecture/:id', function(req, res, next) {
                     else
                         memnum = '0/' + rows[0].memnum;
 
-                    res.render('lecture', { quarters: quarters, ids: ids, lid: lid, title: title, author: author, condition: condition, howto: howto, members: members, memnum: memnum,
+                    res.render('lecture', { quarters: quarters, ids: ids, lid: lid, kind:kind, title: title, author: author, condition: condition, howto: howto, members: members, memnum: memnum,
                         one: one, two: two, three: three, four: four, five: five, six: six, seven: seven, eight: eight, purpose: purpose, etc: etc
                     });
                 }
@@ -337,27 +342,30 @@ router.get('/createLecture/:id', function(req, res, next) {
 router.post('/modifyLecture', function(req, res, next) {
     var body = req.body;
     var lid = body.LectureId;
-    var title = body.Title;
-    var author = body.Author;
-    var condition = body.Condition;
-    var howto = body.Howto;
+    var kind = body.Kind;
+    var title = body.Title.replace(/'/g, '"');
+    var author = body.Author.replace(/'/g, '"');
+    var condition = body.Condition.replace(/'/g, '"');
+    var howto = body.Howto.replace(/'/g, '"');
     var memnum = body.Memnum;
-    var one = body.One;
-    var two = body.Two;
-    var three = body.Three;
-    var four = body.Four;
-    var five = body.Five;
-    var six = body.Six;
-    var seven = body.Seven;
-    var eight = body.Eight;
-    var etc = body.Etc;
-    var purpose = body.Purpose;
+    var one = body.One.replace(/'/g, '"');
+    var two = body.Two.replace(/'/g, '"');
+    var three = body.Three.replace(/'/g, '"');
+    var four = body.Four.replace(/'/g, '"');
+    var five = body.Five.replace(/'/g, '"');
+    var six = body.Six.replace(/'/g, '"');
+    var seven = body.Seven.replace(/'/g, '"');
+    var eight = body.Eight.replace(/'/g, '"');
+    var etc = body.Etc.replace(/'/g, '"');
+    var purpose = body.Purpose.replace(/'/g, '"');
+
+    console.log(purpose)
 
     var db = new sqlite3.Database('db/mydb.db');
 
-    var sql_stmt = 'UPDATE lectures SET title="'+title+'", author="' + author + '", condition="' + condition + '", howto="'+ howto + '", memnum='+ memnum +
-        ', one="' + one + '", two="' + two + '", three="' + three + '", four="' + four + '", five="' + five + '", six="' + six + '", seven="' + seven +
-        '", eight="' + eight + '", etc="' + etc + '", purpose="'+ purpose + '" where rowid='+lid;
+    var sql_stmt = "UPDATE lectures SET title='" + title + "', author='" + author + "', kind='" + kind + "', condition='" + condition + "', howto='" + howto + "', memnum=" + memnum +
+        ", one='" + one + "', two='" + two + "', three='" + three + "', four='" + four + "', five='" + five + "', six='" + six + "', seven='" + seven +
+        "', eight='" + eight + "', etc='" + etc + "', purpose='" + purpose + "' where rowid=" + lid;
     //console.log(sql_stmt)
     db.run(sql_stmt);
     db.close();
@@ -377,6 +385,7 @@ router.get('/modifyLecture/:id', function(req, res, next) {
             res.redirect('/');
         }
         else {
+            var kind = rows[0].kind;
             var title = rows[0].title;
             var author = rows[0].author;
             var condition = rows[0].condition;
@@ -392,7 +401,16 @@ router.get('/modifyLecture/:id', function(req, res, next) {
             var eight = rows[0].eight;
             var etc = rows[0].etc;
             var purpose = rows[0].purpose;
-            res.render('modifyLecture', {lid:lid, title:title, author:author, condition:condition, howto:howto, memnum:memnum, one:one, two:two, three:three, four:four, five:five, six:six, seven:seven, eight:eight, etc:etc, purpose:purpose});
+
+            var kind_selected1 = 'selected';
+            var kind_selected2 = 'selected';
+
+            if (kind == '기획필수') {
+                console.log(kind);
+                kind_selected2 = '';
+            }
+
+            res.render('modifyLecture', {lid:lid, title:title, author:author, condition:condition, howto:howto, memnum:memnum, one:one, two:two, three:three, four:four, five:five, six:six, seven:seven, eight:eight, etc:etc, purpose:purpose, kind_selected1:kind_selected1, kind_selected2:kind_selected2});
         }
     });
 });
@@ -400,27 +418,28 @@ router.get('/modifyLecture/:id', function(req, res, next) {
 router.post('/createLecture', function(req, res, next) {
     var body = req.body;
     var quarter_id = body.QuarterId;
-    var title = body.Title;
-    var author = body.Author;
-    var condition = body.Condition;
-    var howto = body.Howto;
+    var kind = body.Kind;
+    var title = body.Title.replace(/'/g, '"');
+    var author = body.Author.replace(/'/g, '"');
+    var condition = body.Condition.replace(/'/g, '"');
+    var howto = body.Howto.replace(/'/g, '"');
     var memnum = body.Memnum;
-    var one = body.One;
-    var two = body.Two;
-    var three = body.Three;
-    var four = body.Four;
-    var five = body.Five;
-    var six = body.Six;
-    var seven = body.Seven;
-    var eight = body.Eight;
-    var etc = body.Etc;
-    var purpose = body.Purpose;
+    var one = body.One.replace(/'/g, '"');
+    var two = body.Two.replace(/'/g, '"');
+    var three = body.Three.replace(/'/g, '"');
+    var four = body.Four.replace(/'/g, '"');
+    var five = body.Five.replace(/'/g, '"');
+    var six = body.Six.replace(/'/g, '"');
+    var seven = body.Seven.replace(/'/g, '"');
+    var eight = body.Eight.replace(/'/g, '"');
+    var etc = body.Etc.replace(/'/g, '"');
+    var purpose = body.Purpose.replace(/'/g, '"');
 
     var db = new sqlite3.Database('db/mydb.db');
-    var sql_stmt =  quarter_id + ", '" + title + "', '" + author + "', '" + condition +
+    var sql_stmt =  quarter_id + ", '" + kind + ", '"+ title + "', '" + author + "', '" + condition +
         "', '" + howto + "', '', " + memnum + ", '" + one +  "', '" + two +  "', '" + three +
         "', '" + four + "', '" + five + "', '" + six +  "', '" + seven + "', '" + eight + "', '" + etc + "', '" + purpose + "'";
-    db.run("INSERT into lectures(quarter_id, title, author, condition, howto, members, memnum, one, two, three, four, five, six, seven, eight, etc, purpose) VALUES ("+ sql_stmt+ ")", function (err){
+    db.run("INSERT into lectures(quarter_id, kind, title, author, condition, howto, members, memnum, one, two, three, four, five, six, seven, eight, etc, purpose) VALUES ("+ sql_stmt+ ")", function (err){
         //console.log(sql_stmt);
         if (err) {
             console.log('do not create lectrue')
@@ -454,7 +473,7 @@ router.get('/init', function(req, res, next) {
 
     db.serialize(function() {
         db.run("CREATE TABLE if not exists quarters(quarter TEXT UNIQUE)");
-        db.run("CREATE TABLE if not exists lectures(quarter_id INTEGER, title TEXT, author TEXT, condition TEXT, howto TEXT, members TEXT, memnum INTEGER, one TEXT, two TEXT, three TEXT, four TEXT, five TEXT, six TEXT, seven TEXT, eight TEXT, etc TEXT, purpose TEXT)");
+        db.run("CREATE TABLE if not exists lectures(quarter_id INTEGER, title TEXT, author TEXT, condition TEXT, howto TEXT, members TEXT, memnum INTEGER, one TEXT, two TEXT, three TEXT, four TEXT, five TEXT, six TEXT, seven TEXT, eight TEXT, etc TEXT, purpose TEXT, kind TEXT)");
     });
 
     db.close();
